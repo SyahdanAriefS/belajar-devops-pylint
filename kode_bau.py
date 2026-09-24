86cc3444ceafeb15497eba2e37e0a4dce8f83e70
SyahdanAriefS <syahdanariefs@gmail.com>
2026-09-24 11:40:02 +0700

    percobaan kode kualitas tinggi

--- kode_bau.py
+++ kode_bau.py
@@ -1,15 +1,24 @@
-import os, sys, math
+X_VALUE = 10
 
-x = 10
+def process_data(val_a, val_b, val_c, val_d, list_e, val_f):
+    """
+    Fungsi kalkulasi sederhana berdasarkan kondisi boolean dan validasi data.
+    """
 
-def Bad_Function_Name( A, B, C, D, E, F ):
- global x
- l = 1; O = 0
- if A == True:
-  if B == False:
-   if C == None:
-    try: print(eval("A + B")); res = E[0] + F + l + O
-    except: pass
- else: return None
+    var_l = 1
+    var_o = 0
 
-Bad_Function_Name(True, False, None, 1, [2], 3)
+    if val_a and not val_b and val_c is None:
+        try:
+            print(val_a + val_b)
+
+            result = list_e[0] + val_f + var_l + var_o
+            return result
+        except (IndexError, TypeError, ValueError) as error:
+            print(f"Terjadi kesalahan komputasi: {error}")
+            return None
+
+    return None
+
+if __name__ == "__main__":
+    process_data(True, False, None, 1, [2], 3)
